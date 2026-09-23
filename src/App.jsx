89939f6a68a31ad969a6d5e9731748f31da60507
Stephen Pleasant <stephenpleasant1@gmail.com>
2026-09-23 10:21:20 +0100

    Fix footer import case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import AboutSection from './components/AboutSection';
 import SkillsSection from './components/SkillsSection';
 import ProjectsSection from './components/ProjectsSection';
 import ContactSection from './components/ContactSection';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 import './index.css';
 
 const Portfolio = () => {
